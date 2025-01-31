fb1c43d9d643daebad1f27386a8e1e04b37eb6a2
JoaoPedro <jpsueiro2018@gmail.com>
2025-01-31 12:27:06 -0300

    aula-01

---
+++ Javascipt/Aula-01/script.js
@@ -0,0 +1,26 @@
+console.log("Ola Mundo");
+
+//alert//
+alert("Ola mundo 1");
+
+//confirm//
+    var teste = confirm("Ola mundo 2");
+    //Exibindo a opção escolhida pelo usuário
+    console.log(teste)
+
+//exemplo de Prompt//
+    var testePrompt = prompt("Digite seu nome")
+    //exibindo o nome do usuário
+    console.log(testePrompt)
+    alert(testePrompt)
+
+//Tentativa de adição
+
+    var numero1 = Number.parseInt(prompt("Digite o primeiro numero"))
+    var numero2 = Number.parseInt(prompt("Digite o segundo numero"))
+    //Verificando o tipo da variavel
+    console.log(typeof(numero1))
+    alert(numero1 + numero2)
+
+//
+
