c04742c11158689aaae2115e2a036519b13f8629
JoaoPedro <jpsueiro2018@gmail.com>
2025-01-31 15:14:28 -0300

    Atividades

---
+++ Javascipt/Aula-01/Primeira atividade/Exer.1/script.js
@@ -0,0 +1,4 @@
+console.log("Bom dia mundo!!!");
+
+//alert//
+alert("Bom dia mundo!!!");
---
+++ Javascipt/Aula-01/Primeira atividade/Exer.3/script.js
@@ -0,0 +1,17 @@
+
+
+var numero1 = Number.parseInt(prompt("Digite o primeiro numero"))
+var numero2 = Number.parseInt(prompt("Digite o segundo numero"))
+//Verificando o tipo da variavel
+console.log(typeof(numero1))
+alert(numero1 + numero2)
+console.log(typeof(numero1))
+alert(numero1 - numero2)
+console.log(typeof(numero1))
+alert(numero1 * numero2)
+console.log(typeof(numero1))
+alert(numero1 / numero2)
+
+
+//
+
